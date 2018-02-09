d8374bb70c7d5aab88edd10a9897ad478e42b8c1
mengfanxing <479922976@qq.com>
2018-02-09 16:48:02 +0800

    asd

---
+++ src/main/java/com/springboot/study/pojo/user/User.java
@@ -0,0 +1,13 @@
+package com.springboot.study.pojo.user;
+
+/**
+ * Copyright © 2018 宫宇祁. All rights reserved.
+ * <p>
+ * ${DESCRIPTION}
+ *
+ * @create 2018-02-09 16:47
+ * @author: 宫宇祁
+ * @ModificationHistory Who     When      What
+ */
+public class User {
+}
